54bd6c1c0ec1128058df7bca7be5d3fc2ef46b9f
Charles Zeng <cjzeng@users.noreply.github.com>
2017-06-23 21:16:48 -0400

    Update test.vb

--- Project2/test.vb
+++ Project2/test.vb
@@ -1,11 +1,9 @@
-Option Explicit
-
 Sub testSourceMap()
     Dim src As New SourceMap
     src.setColumnName 1, "first"
     src.setColumnName 2, "second"
-    Debug.Print "idx 1=", src.getColumnName, 1
-    Debug.Print "idx 2=", src.getColumnName, 2
+    Debug.Print "idx 1=", src.getColumnName(1)
+    Debug.Print "idx 2=", src.getColumnName(2)
 
 End Sub
 
